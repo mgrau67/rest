8622bfd7243b31d15d1ec0329dcad9a59bc8e703
mgrau67 <mgrau67@gmail.com>
2020-04-03 19:41:16 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Text;
+using System.Configuration;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+
+namespace rest
+{
+    class Program
+    {
+        static string m_clientRestDocService = "/LATEST/search"; // MarkLogic REST API service for document search.
+        static string m_clientRestEvalService = "/LATEST/eval"; // MarkLogic REST API service for eval code.
+        static string m_host = "";
+        static string m_port = "";
+        static HttpClient m_httpClient;
+
+        static void Main(string[] args)
+        {
+            m_host = ConfigurationManager.AppSettings["host"];
+            m_port = ConfigurationManager.AppSettings["port"];
+            var username = ConfigurationManager.AppSettings["username"];
+            var password = ConfigurationManager.AppSettings["password"];
+            var realm = ConfigurationManager.AppSettings["realm"];
+
+            // create connection
+            CredentialCache m_credCache = new CredentialCache();
+            HttpClientHandler m_clientHandler = new HttpClientHandler();
+            string mlHost = string.Format("http://{0}:{1}", m_host, m_port);
+            m_credCache.Add(new Uri(mlHost), "Digest", new NetworkCredential(username, password, realm));
+            m_clientHandler.Credentials = m_credCache;
+            m_clientHandler.PreAuthenticate = true;
+            m_httpClient = new HttpClient(m_clientHandler);
+
+            // read parameters
+            string service = args[0];
+            string param = args[1];
+            string result = "";
+            switch (service)
+            {
+                case "search":
+                    result = serviceSearch(param);
+                    break;
+                case "eval":
+                    result = serviceEval(param);
+                    break;
+            }
+            Console.WriteLine(result);
+        }
+
+        static private string serviceSearch(string _text)
+        {
+            // build url
+            string query = _text;
+            int start = 1;
+            int pageLength = 10;
+            string format = "xml";
+            string url = string.Format("http://{0}:{1}{2}?q={3}&start={4}&pageLength={5}&format={6}",
+                                       m_host, m_port, m_clientRestDocService,
+                                       query, start, pageLength, format);            
+            Uri requestUri = new Uri(url);
+
+            // call url
+            HttpResponseMessage response = m_httpClient.GetAsync(requestUri).Result;
+            string result = "";
+            if (response.IsSuccessStatusCode)
+            {
+                result = response.Content.ReadAsStringAsync().Result;
+            }
+            else
+            {
+                result = string.Format("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+            return result;
+        }
+
+        static private string serviceEval(string _file)
+        {
+            // build url
+            string url = string.Format("http://{0}:{1}{2}",
+                                        m_host, m_port, m_clientRestEvalService);
+            Uri requestUri = new Uri(url);
+            var request = new HttpRequestMessage(new HttpMethod("POST"), requestUri);
+            request.Content = new StringContent(Regex.Replace(File.ReadAllText(_file), "(?:\\r\\n|\\n|\\r)", string.Empty));
+            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+
+            // call url
+            HttpResponseMessage response = m_httpClient.SendAsync(request).Result;
+            string result = "";
+            if (response.IsSuccessStatusCode)
+            {
+                result = response.Content.ReadAsStringAsync().Result;
+            }
+            else
+            {
+                result = string.Format("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("rest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Federico Joly y Cía. S.A.")]
+[assembly: AssemblyProduct("rest")]
+[assembly: AssemblyCopyright("Copyright © Federico Joly y Cía. S.A. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("bc678e69-84e6-477f-8a19-0561fd174e13")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
